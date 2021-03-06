41b52e9300831244a2633bc8bc7c4822a4162fca
smorales95 <steven_pibe95@hotmail.com>
2021-03-06 06:46:03 +0000

    Prueba

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -5,7 +5,7 @@ export function Footer() {
 	return (
 		<footer className="bg-dark text-center text-lg-start">
 			<div className="text-center text-light fot p-3">
-				© 2021 Copyright:
+				© 2021 Copyright
 			</div>
 		</footer>
 	);
